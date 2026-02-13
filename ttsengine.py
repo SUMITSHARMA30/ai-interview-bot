dcc85391eac42c09435aa83da044bf6fa7a34326
Sumit Sharma <sumit81998383@gmail.com>
2026-02-13 20:33:12 +0530

    deleted duplicate files

--- ttsengine.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import tempfile
-
-def text_to_speech(text):
-    tts = gTTS(text)
-    temp_audio = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3")
-    tts.save(temp_audio.name)
-    return temp_audio.name
